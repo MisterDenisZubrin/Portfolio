60761c8b7313d313041b37e3624c37b56bd83271
Denis <dendel1998@gmail.com>
2022-07-20 14:59:10 +0300

    Добавлены свежие проекты

---
+++ JavaScript/Таблица цветов с веб-компонентами (бета)/js/main.js
@@ -0,0 +1,5 @@
+import ColorTable from "./components/table.js";
+import ColorPicker from "./components/form.js";
+
+customElements.define('color-table', ColorTable);
+customElements.define('color-picker', ColorPicker);
